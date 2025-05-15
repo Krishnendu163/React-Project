b01752e4dc1be5b8f22b8fcb47d1628c02597704
mejora79 <admin@mejorainfotech.com>
2025-05-15 19:06:31 +0530

    Video number 16 || React Routing

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,42 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import TextFroms from "./pages/TextFroms";
 
-const Routes = () =>{
-    const router = createBrowserRouter([
-       {
-            path: "/about",
-            element: <About/>
-        },
-       
-    ])
+// Layout component that wraps the navbar and the route's outlet
+const Layout = () => (
+  <>
+    <Navbar />
+    <main className="my-4">
+    <Outlet />
+    </main>
+  </>
+);
 
-    return router;
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />, // Wrap all child routes with Layout
+    children: [
+      {
+        index: true, // This represents the root path "/"
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "text-utils",
+        element: <TextFroms />,
+      },
+    ],
+  },
+]);
+
+const Routes = () => {
+  return <RouterProvider router={router} />;
 };
 
 export default Routes;
